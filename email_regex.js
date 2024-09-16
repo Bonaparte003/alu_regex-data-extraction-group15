23b525c872af0a80748b76556f747642810e3757
BirasaDivine <b.divine@alustudent.com>
2024-09-16 12:11:15 +0200

    Regex email

---
+++ email_regex.js
@@ -0,0 +1,5 @@
+export function email_checker(text) {
+    let email_regex = /[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}/g;
+    let results = text.match(email_regex);
+    return results || [];
+}
